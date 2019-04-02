9a639a1993009d3d1665ae540b84faf2b2ac8988
Asok P <asok@fastmail.com>
2019-04-03 05:24:28 +0530

    Initial check in of plugin boilerplate created from https://wppb.me

---
+++ includes/class-fetch-mailchimp-fields-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       wordpress.org
+ * @since      1.0.0
+ *
+ * @package    Fetch_Mailchimp_Fields
+ * @subpackage Fetch_Mailchimp_Fields/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Fetch_Mailchimp_Fields
+ * @subpackage Fetch_Mailchimp_Fields/includes
+ * @author     Asok P <asok@fastmail.com>
+ */
+class Fetch_Mailchimp_Fields_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
